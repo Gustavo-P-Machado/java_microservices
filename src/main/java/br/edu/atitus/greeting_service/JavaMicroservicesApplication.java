a7e5bf56d487267fb62a451a503a192bfeecc072
Gustavo Pan <pangustavo34@gmail.com>
2025-09-05 22:07:52 -0300

    Criação de repositório, Controllers, Configs e application.properties

---
+++ src/main/java/br/edu/atitus/greeting_service/JavaMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package br.edu.atitus.greeting_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMicroservicesApplication.class, args);
+    }
+
+}
