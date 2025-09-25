ca8136135667d516b5ed3aa825d48b9718cf5a9c
Gustavo Pan <pangustavo34@gmail.com>
2025-09-25 20:58:21 -0300

    Criação Product Service - 25/09

---
+++ product-service/src/main/java/br/edu/atitus/product_service/entities/ProductEntity.java
@@ -0,0 +1,99 @@
+package br.edu.atitus.product_service.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_product")
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private String brand;
+    private String model;
+    private double price;
+    @Column(length = 3)
+    private String currency; //Diz QUAL a moeda o produto está salvo
+    private int stock;
+
+    @Transient
+    private String environment;
+    @Transient
+    private double convertedPrice;
+
+    //Getters e Setters
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+
+    public double getConvertedPrice() {
+        return convertedPrice;
+    }
+
+    public void setConvertedPrice(double convertedPrice) {
+        this.convertedPrice = convertedPrice;
+    }
+}
---
+++ product-service/src/main/resources/db_migration/V2__PopulateTbProduct.sql
@@ -0,0 +1,5 @@
+insert into tb_product (description, brand, model, currency, price, stock) values
+        ('iPhone 15', 'Apple', 'iPhone 15 256GB', 'USD', 1022.96, 10),
+        ('Moto G85', 'Motorola', 'Moto G85 256GB', 'USD', 306.78, 10),
+        ('Redmi 13C', 'Xiaomi', 'Redmi 13C 256GB', 'USD', 242.15, 10),
+        ('S23 Ultra', 'Samsung', 'S23 Ultra 256GB', 'USD', 768.70, 10);
